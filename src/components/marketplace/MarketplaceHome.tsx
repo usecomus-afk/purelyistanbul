db84585216d9a8654cb84e2a2ec4474a5a4c88ab
Use Comus <usecomus@gmail.com>
2026-09-12 11:21:37 +0300

    style: fix skyline overlap and use rounded square logo in header

--- src/components/marketplace/MarketplaceHome.tsx
+++ src/components/marketplace/MarketplaceHome.tsx
@@ -157,7 +157,7 @@ export function MarketplaceHome() {
         </div>
 
         {/* Yeni Skyline Çizimi */}
-        <div className="relative z-10 w-full max-w-[440px] mx-auto px-6 flex justify-center pointer-events-none mt-4 sm:mt-6 pb-0">
+        <div className="relative z-10 w-full max-w-[380px] mx-auto px-6 flex justify-center pointer-events-none mt-2 sm:mt-4 pb-0">
           <Image
             src="/images/skyline-drawing-transparent.png"
             alt="Nothing but Istanbul"
@@ -170,7 +170,7 @@ export function MarketplaceHome() {
       </section>
 
       {/* Kategori Çubuğu */}
-      <div className="relative z-20 -mt-6 sm:-mt-12">
+      <div className="relative z-20 mt-1 sm:mt-3">
         <CategoryFilterBar value={category} onChange={setCategory} />
       </div>
 
