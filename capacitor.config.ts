3ebf0a2b0cd2826384e4906b6fdc5108988fc19c
Use Comus <usecomus@gmail.com>
2026-09-26 21:53:28 +0300

    chore: update bundle ID to match App Store Connect

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.xenios.istanbul',
+  appId: 'com.usecomus.olympus',
   appName: 'purelyİstanbul',
   webDir: 'public',
   server: {
